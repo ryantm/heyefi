ee9185fac2c25999c581cdd6f485ee9a8c91f036
Ryan Mulligan <ryan@ryantm.com>
2015-04-12 14:36:32 -0700

    add logging function

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@ module Main where
 
 import HEyefi.Constant
 
+import Data.Time.Clock
+import Data.Time.ISO8601
 import Network.Wai ( responseLBS
                    , Application
                    , requestMethod
@@ -14,15 +16,18 @@ import Network.HTTP.Types.Header (hContentType)
 import Network.HTTP.Types.Method (Method (..))
 
 
+
+logInfo :: String -> IO ()
+logInfo s = do
+  t <- getCurrentTime
+  putStrLn (unwords ["[" ++ formatISO8601Millis t ++ "]", "[INFO]", s])
+
 main = do
     putStrLn $ "Listening on port " ++ show port
     run port app
 
 app :: Application
 app req f
-  | requestMethod req == "GET" =
-      putStrLn (show (requestHeaders req)) >>
-      (f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!")
   | requestMethod req == "POST" =
-      putStrLn (show (requestHeaders req)) >>
+      logInfo (show (requestHeaders req)) >>
       (f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!")
