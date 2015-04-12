9bd2ef95c821b5f58ec84f0b51c481f43b758b78
Ryan Mulligan <ryan@ryantm.com>
2015-04-12 07:10:54 -0700

    add basic minimal http server
    It listens on the correct port for my Eye-Fi card. It sees a SOAP POST
    request when the card powers on.

--- Main.hs
+++ Main.hs
@@ -1,1 +1,28 @@
-main = putStrLn "hello"
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import HEyefi.Constant
+
+import Network.Wai ( responseLBS
+                   , Application
+                   , requestMethod
+                   , requestHeaders )
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
+import Network.HTTP.Types.Method (Method (..))
+
+
+main = do
+    putStrLn $ "Listening on port " ++ show port
+    run port app
+
+app :: Application
+app req f
+  | requestMethod req == "GET" =
+      putStrLn (show (requestHeaders req)) >>
+      (f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!")
+  | requestMethod req == "POST" =
+      putStrLn (show (requestHeaders req)) >>
+      (f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!")
