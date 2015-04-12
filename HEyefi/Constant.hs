8f81eb0b7fc4fd52246c2fbd90f14ab50b478f62
Ryan Mulligan <ryan@ryantm.com>
2015-04-12 07:09:39 -0700

    add configuration as constants
    Temporarily add configuration as constants for development ease.

---
+++ HEyefi/Constant.hs
@@ -0,0 +1,17 @@
+module HEyefi.Constant where
+
+port :: Int
+port = 59278
+
+mac_0 = "0018562de4ce"
+upload_key_0 = "36d61e4e7403a0586702c9159892a062"
+upload_dir = "/data/annex/doxie/unsorted"
+upload_uid = 1000
+upload_gid = 1000
+upload_file_mode = 420
+upload_dir_mode = 509
+
+
+geotag_enable = 1
+geotag_lag = 3600
+geotag_accuracy = 140000
