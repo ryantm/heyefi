b27d9e002993c031ecccaa5fffcfd530628f0861
Ryan Mulligan <ryan@ryantm.com>
2015-04-22 07:33:09 -0700

    WIP: trying to get xml processing instruction to appear at beginning of string

--- src/HEyefi/StartSession.hs
+++ src/HEyefi/StartSession.hs
@@ -8,11 +8,26 @@ import Text.XML.HXT.Core ( runX
                          , mkelem
                          , sattr
                          , txt
-                         , root
-                         , writeDocumentToString )
+                         , root, writeDocument)
 import Control.Arrow ((>>>))
 import Data.Hash.MD5 (md5s, Str (..))
 import Data.Hex (unhex)
+
+import Text.XML.HXT.Core ( prepareContents, withOutputEncoding, unicodeString, encodeDocument', xshow, getChildren, ArrowXml (..), SysConfigList, XmlTree, withOutputXML, withXmlPi, yes)
+import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
+                                                ( initialSysState)
+
+writeDocumentToString'   :: ArrowXml a => SysConfigList  -> a XmlTree String
+writeDocumentToString' config
+    = prepareContents ( foldr (>>>) id (withXmlPi yes :
+                                        withOutputEncoding unicodeString :
+                                        withOutputXML :
+                                        config
+                                       )
+                        $ initialSysState
+                      ) encodeDocument'
+      >>>
+      xshow getChildren
 
 
 
@@ -33,8 +48,9 @@ startSessionResponse macaddress cnonce transfermode transfermodetimestamp = do
             ]
           ]
         ]
-  result <- (runX (root [] [document] >>> writeDocumentToString []))
-  return (head result)
+  _ <- (runX (root [] [document] >>> writeDocument [] ""))
+  result <- (runX (root [] [document] >>> writeDocumentToString' []))
+  return (show result)
   where
     credential bcs = md5s (Str (bcs))
     binaryCredentialString = unhex credentialString
