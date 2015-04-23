b73555778f1aa44f9679b1f762569f5bfd4faa8d
Ryan Mulligan <ryan@ryantm.com>
2015-04-23 07:05:04 -0700

    add xml processing instruction, test passes

--- src/HEyefi/StartSession.hs
+++ src/HEyefi/StartSession.hs
@@ -6,51 +6,38 @@ import HEyefi.Constant (upload_key_0)
 
 import Text.XML.HXT.Core ( runX
                          , mkelem
+                         , spi
+                         , t_xml
                          , sattr
                          , txt
-                         , root, writeDocument)
+                         , root
+                         , writeDocumentToString)
 import Control.Arrow ((>>>))
 import Data.Hash.MD5 (md5s, Str (..))
 import Data.Hex (unhex)
-
-import Text.XML.HXT.Core ( prepareContents, withOutputEncoding, unicodeString, encodeDocument', xshow, getChildren, ArrowXml (..), SysConfigList, XmlTree, withOutputXML, withXmlPi, yes)
-import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow
-                                                ( initialSysState)
-
-writeDocumentToString'   :: ArrowXml a => SysConfigList  -> a XmlTree String
-writeDocumentToString' config
-    = prepareContents ( foldr (>>>) id (withXmlPi yes :
-                                        withOutputEncoding unicodeString :
-                                        withOutputXML :
-                                        config
-                                       )
-                        $ initialSysState
-                      ) encodeDocument'
-      >>>
-      xshow getChildren
-
-
 
 startSessionResponse :: String -> String -> String -> String -> IO String
 startSessionResponse macaddress cnonce transfermode transfermodetimestamp = do
   bcs <- binaryCredentialString
   let document =
-        mkelem "SOAP-ENV:Envelope"
-        [ sattr "xmlns:SOAP-ENV" "http://schemas.xmlsoap.org/soap/envelope/" ]
-        [ mkelem "SOAP-ENV:Body" []
-          [ mkelem "StartSessionResponse"
-            [ sattr "xmlns" "http://localhost/api/soap/eyefilm" ]
-            [ mkelem "credential" [] [ txt (credential bcs) ]
-            , mkelem "snonce" [] [ txt "340282366920938463463374607431768211456" ]
-            , mkelem "transfermode" [] [ txt transfermode ]
-            , mkelem "transfermodetimestamp" [] [ txt transfermodetimestamp ]
-            , mkelem "upsyncallowed" [] [ txt "true" ]
+        root []
+        [ spi t_xml "version=\"1.0\" encoding=\"UTF-8\""
+        , mkelem "SOAP-ENV:Envelope"
+          [ sattr "xmlns:SOAP-ENV" "http://schemas.xmlsoap.org/soap/envelope/" ]
+          [ mkelem "SOAP-ENV:Body" []
+            [ mkelem "StartSessionResponse"
+              [ sattr "xmlns" "http://localhost/api/soap/eyefilm" ]
+              [ mkelem "credential" [] [ txt (credential bcs) ]
+              , mkelem "snonce" [] [ txt "bff7fe782919114202d3b601682ba8aa" ]
+              , mkelem "transfermode" [] [ txt transfermode ]
+              , mkelem "transfermodetimestamp" [] [ txt transfermodetimestamp ]
+              , mkelem "upsyncallowed" [] [ txt "true" ]
+              ]
             ]
           ]
         ]
-  _ <- (runX (root [] [document] >>> writeDocument [] ""))
-  result <- (runX (root [] [document] >>> writeDocumentToString' []))
-  return (show result)
+  result <- runX (document >>> writeDocumentToString [])
+  return (head result)
   where
     credential bcs = md5s (Str (bcs))
     binaryCredentialString = unhex credentialString
