e3e49b67df263e6375b9215cd96b70ed016eb0e6
Ryan Mulligan <ryan@ryantm.com>
2015-05-23 22:49:29 -0700

    add test for properly parsing configuration card

--- test/HEyefi/ConfigSpec.hs
+++ test/HEyefi/ConfigSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module HEyefi.ConfigSpec where
@@ -5,12 +6,13 @@ module HEyefi.ConfigSpec where
 import Test.Hspec
 
 import Control.Exception (catch, SomeException, throwIO)
-import System.IO.Silently (capture_)
+import System.IO.Silently (capture, capture_)
 import System.FilePath ((</>))
 import System.Directory (getTemporaryDirectory)
 
 import HEyefi.Config
 
+import qualified Data.HashMap.Strict as HM
 import System.Directory (removeFile)
 import System.IO.Error (isDoesNotExistError)
 
@@ -27,11 +29,17 @@ getNonexistentTemporaryFile = do
   removeIfExists file
   return file
 
-makeAndReloadFile :: String -> IO String
+makeAndReloadFile_ :: String -> IO String
+makeAndReloadFile_ s = fmap fst (makeAndReloadFile s)
+
+makeAndReloadFile :: String -> IO (String, Config)
 makeAndReloadFile contents = do
   file <- getNonexistentTemporaryFile
   writeFile file contents
-  capture_ (reloadConfig file)
+  capture (reloadConfig file)
+
+validConfig :: String
+validConfig = "upload_dir = \"/data/photos\"\ncards = [[\"0012342de4ce\",\"e7403a0123402ca062\"],[\"1234562d5678\",\"12342a062\"]]"
 
 spec :: Spec
 spec = do
@@ -43,10 +51,14 @@ spec = do
          output `shouldContain` "Could not find configuration file at " ++ file))
     (it "should report an error for an unparsable configuration file"
      (do
-         output <- makeAndReloadFile "a = (\n"
+         output <- makeAndReloadFile_ "a = (\n"
          output `shouldContain` "Error parsing configuration file at "
          output `shouldContain` "with message: endOfInput"))
     (it "should complain about missing cards configuration"
      (do
-         output <- makeAndReloadFile "upload_dir = \"/data/annex/doxie/unsorted\""
-         output `shouldContain` "missing a definition for `cards`.")))
+         output <- makeAndReloadFile_ "upload_dir = \"/data/annex/doxie/unsorted\""
+         output `shouldContain` "missing a definition for `cards`."))
+    (it "should parse cards for a validConfig"
+     (do
+         (_, config) <- makeAndReloadFile validConfig
+         HM.lookup "0012342de4ce" (cardMap config) `shouldBe` (Just "e7403a0123402ca062"))))
