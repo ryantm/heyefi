2a29849789b443ad0d32756d8d0f37db01751111
Ryan Mulligan <ryan@ryantm.com>
2015-05-31 07:32:10 -0700

    add errors package

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, bytestring, case-insensitive, configurator
 , containers, directory, filepath, HandsomeSoup, hspec, HTTP
 , http-types, hxt, iso8601-time, MissingH, multipart, old-locale
-, silently, stdenv, stm, tar, text, time, unix
+, silently, stdenv, stm, tar, text, time, unix, errors
 , unordered-containers, utf8-string, wai, warp, ghc, cabal-install
 }:
 mkDerivation {
@@ -14,13 +14,13 @@ mkDerivation {
   buildDepends = [
     base bytestring case-insensitive configurator HandsomeSoup HTTP
     http-types hxt iso8601-time MissingH multipart old-locale stm tar
-    text time unix unordered-containers utf8-string wai warp
+    text time unix unordered-containers utf8-string wai warp errors
   ];
   testDepends = [
     base bytestring case-insensitive configurator containers directory
     filepath HandsomeSoup hspec HTTP http-types hxt iso8601-time
     MissingH multipart old-locale silently stm tar text time unix
-    unordered-containers utf8-string wai warp
+    unordered-containers utf8-string wai warp errors
   ];
   homepage = "https://github.com/ryantm/heyefi";
   description = "A server for Eye-Fi SD cards written in Haskell. This project is not endorsed by Eye-Fi Inc.";
