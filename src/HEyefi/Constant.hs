c6d5ddf423aaf9a92715c8aba99bda77c1071fcf
Ryan Mulligan <ryan@ryantm.com>
2015-06-09 06:54:50 -0700

    remove unwanted commented-out constants

--- src/HEyefi/Constant.hs
+++ src/HEyefi/Constant.hs
@@ -6,14 +6,6 @@ port = 59278
 configPath :: String
 configPath = "/etc/heyefi/heyefi.config"
 
--- mac_0 :: String
--- mac_0 = "0018562de4ce"
-
--- upload_key_0 :: String
--- upload_key_0 = "36d61e4e7403a0586702c9159892a062"
--- upload_dir :: String
--- upload_dir = "/data/annex/doxie/unsorted"
-
 -- upload_uid :: Int
 -- upload_uid = 1000
 -- upload_gid :: Int
@@ -22,13 +14,6 @@ configPath = "/etc/heyefi/heyefi.config"
 -- upload_file_mode = 420
 -- upload_dir_mode :: Int
 -- upload_dir_mode = 509
-
--- geotag_enable :: Int
--- geotag_enable = 1
--- geotag_lag :: Int
--- geotag_lag = 3600
--- geotag_accuracy :: Int
--- geotag_accuracy = 140000
 
 multipartBodyBoundary :: String
 multipartBodyBoundary =
