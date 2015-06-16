4893bbced21d6409ae9eddf09635c56d3ed72060
Ryan Mulligan <ryan@ryantm.com>
2015-06-16 07:09:23 -0700

    work on converting to Reader monad

--- src/HEyefi/Config.hs
+++ src/HEyefi/Config.hs
@@ -2,13 +2,14 @@
 
 module HEyefi.Config where
 
-import           HEyefi.Types (Config(..), CardConfig, SharedConfig(..), LogLevel(Info), cardMap, uploadDirectory, logLevel)
+import           HEyefi.Types (Config(..), CardConfig, SharedConfig(..), LogLevel(Info), cardMap, uploadDirectory, logLevel, HEyefiM(..))
 import           HEyefi.Log (logInfo)
 import           HEyefi.Constant hiding (configPath)
 
 import           Control.Concurrent.STM (TVar, readTVar, newTVar, writeTVar, atomically, retry)
 import           Control.Exception (SomeException (..))
 import           Control.Exception (finally, catches, Handler (..))
+import           Control.Monad.IO.Class (liftIO)
 import           Data.Configurator (load, Worth (Required), getMap)
 import           Data.Configurator.Types (Value, ConfigError (ParseError))
 import qualified Data.Configurator.Types as CT
@@ -42,17 +43,17 @@ convertCardList (CT.List innerList) =
     extractTuple _ = Nothing
 convertCardList _ = Left cardsFormatDoesNotMatch
 
-getCardConfig :: LogLevel -> HM.HashMap CT.Name CT.Value -> IO CardConfig
-getCardConfig globalLogLevel configMap = do
+getCardConfig :: HM.HashMap CT.Name CT.Value -> HEyefiM CardConfig
+getCardConfig configMap = do
   let cards = HM.lookup "cards" configMap
   case cards of
    Nothing -> do
-     logInfo globalLogLevel missingCardsDefinition
+     logInfo missingCardsDefinition
      return HM.empty
    Just l -> do
      case convertCardList l of
       Left msg -> do
-        logInfo globalLogLevel msg
+        logInfo msg
         return HM.empty
       Right cardList ->
         return (HM.fromList cardList)
@@ -63,38 +64,37 @@ convertUploadDirectory (CT.String uploadDir) =
 convertUploadDirectory _ =
   Left uploadDirFormatDoesNotMatch
 
-getUploadDirectory :: LogLevel -> HM.HashMap CT.Name CT.Value -> IO FilePath
-getUploadDirectory globalLogLevel configMap = do
+getUploadDirectory :: HM.HashMap CT.Name CT.Value -> HEyefiM FilePath
+getUploadDirectory configMap = do
   let uploadDir = HM.lookup "upload_dir" configMap
   case uploadDir of
    Nothing -> do
-     logInfo globalLogLevel missingUploadDirDefinition
+     logInfo missingUploadDirDefinition
      return ""
    Just uD -> do
      case convertUploadDirectory uD of
       Left msg -> do
-        logInfo globalLogLevel msg
+        logInfo msg
         return ""
       Right path ->
         return path
 
-reloadConfig :: LogLevel -> FilePath -> IO Config
-reloadConfig globalLogLevel configPath = do
-  logInfo globalLogLevel ("Trying to load configuration at " ++ configPath)
+reloadConfig :: FilePath -> HEyefiM Config
+reloadConfig configPath = do
+  logInfo ("Trying to load configuration at " ++ configPath)
   catches (
     do
-      config <- load [Required configPath]
-      configMap <- getMap config
-      cardConfig <- getCardConfig globalLogLevel configMap
-      uploadDir <- getUploadDirectory globalLogLevel configMap
-      logInfo globalLogLevel "Loaded configuration"
+      config <- liftIO (load [Required configPath])
+      configMap <- liftIO (getMap config)
+      cardConfig <- getCardConfig configMap
+      uploadDir <- getUploadDirectory configMap
+      logInfo "Loaded configuration"
       return Config {
         cardMap = cardConfig,
         uploadDirectory = uploadDir }
     )
     [Handler (\(ParseError p msg) -> do
                  logInfo
-                   globalLogLevel
                    ("Error parsing configuration file at " ++
                           p ++
                           " with message: " ++
@@ -102,7 +102,6 @@ reloadConfig globalLogLevel configPath = do
                  return emptyConfig),
      Handler (\(SomeException _) -> do
                  logInfo
-                   globalLogLevel
                    ("Could not find configuration file at " ++ configPath)
                  return emptyConfig)]
 
@@ -117,14 +116,13 @@ newConfig = atomically (newTVar emptyConfig)
 -- Example config:
 -- cards = [["0012342de4ce","e7403a0123402ca062"],["1234562d5678","12342a062"]]
 -- upload_dir = "/data/annex/doxie/unsorted"
-monitorConfig :: LogLevel ->
-                 FilePath -> SharedConfig -> TVar (Maybe Int) -> IO ()
-monitorConfig globalLogLevel configPath sharedConfig wakeSignal =
-  finally
+monitorConfig :: FilePath -> SharedConfig -> TVar (Maybe Int) -> HEyefiM ()
+monitorConfig configPath sharedConfig wakeSignal =
+  liftIO (finally
     (do
-        config <- reloadConfig globalLogLevel configPath
-        atomically (writeTVar sharedConfig config))
-    (waitForWake wakeSignal)
+        config <- reloadConfig configPath
+        liftIO (atomically (writeTVar sharedConfig config)))
+    (waitForWake wakeSignal))
 
 getUploadKeyForMacaddress :: Config -> String -> Maybe String
 getUploadKeyForMacaddress c mac =
