cded7c44542eb190f43b14daac52f7204aa66e16
Ryan Mulligan <ryan@ryantm.com>
2015-07-13 06:47:47 -0700

    add optparse-applicative as a dependency

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@
 , silently, stdenv, stm, tar, text, time, unix, errors_2_0_0
 , unordered-containers, utf8-string, wai, warp, ghc, cabal-install
 , temporary, filemanip, mtl, transformers, exceptions, random, hlint
+, optparse-applicative
 }:
 mkDerivation {
   pname = "heyefi";
@@ -17,6 +18,7 @@ mkDerivation {
     http-types hxt iso8601-time MissingH multipart old-locale stm tar
     text time unix unordered-containers utf8-string wai warp errors_2_0_0
     temporary filemanip filepath mtl transformers exceptions random
+    optparse-applicative
   ];
   testDepends = [
     base bytestring case-insensitive configurator containers directory
@@ -24,6 +26,7 @@ mkDerivation {
     MissingH multipart old-locale silently stm tar text time unix
     unordered-containers utf8-string wai warp errors_2_0_0
     temporary filemanip mtl transformers exceptions random
+    optparse-applicative
   ];
   homepage = "https://github.com/ryantm/heyefi";
   description = "A server for Eye-Fi SD cards written in Haskell. This project is not endorsed by Eye-Fi Inc.";
