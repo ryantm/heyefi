d2d9dd711ec40de227f74c7b46ea26881eeea9b9
Ryan Mulligan <ryan@ryantm.com>
2015-07-25 08:14:39 -0700

    remove unused Haskell packages

--- default.nix
+++ default.nix
@@ -1,31 +1,32 @@
 { mkDerivation, base, bytestring, case-insensitive, configurator
-, containers, directory, filepath, HandsomeSoup, hspec, HTTP
-, http-types, hxt, iso8601-time, MissingH, multipart, old-locale
-, silently, stdenv, stm, tar, text, time, unix, errors_2_0_0
+, containers, directory, filepath, HandsomeSoup, hspec
+, http-types, hxt, iso8601-time, MissingH, multipart
+, silently, stdenv, stm, tar, text, time, unix
 , unordered-containers, utf8-string, wai, warp, ghc, cabal-install
-, temporary, filemanip, mtl, transformers, exceptions, random, hlint
-, optparse-applicative
+, temporary, mtl, transformers, exceptions, random, hlint
+, optparse-applicative, packunused
 }:
+
 mkDerivation {
   pname = "heyefi";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildTools = [ ghc cabal-install hlint];
+  buildTools = [ ghc cabal-install hlint packunused];
   buildDepends = [
-    base bytestring case-insensitive configurator HandsomeSoup HTTP
-    http-types hxt iso8601-time MissingH multipart old-locale stm tar
-    text time unix unordered-containers utf8-string wai warp errors_2_0_0
-    temporary filemanip filepath mtl transformers exceptions random
+    base bytestring case-insensitive configurator HandsomeSoup
+    http-types hxt iso8601-time MissingH multipart stm tar
+    text time unix unordered-containers utf8-string wai warp
+    temporary filepath mtl transformers exceptions random
     optparse-applicative
   ];
   testDepends = [
     base bytestring case-insensitive configurator containers directory
-    filepath HandsomeSoup hspec HTTP http-types hxt iso8601-time
-    MissingH multipart old-locale silently stm tar text time unix
-    unordered-containers utf8-string wai warp errors_2_0_0
-    temporary filemanip mtl transformers exceptions random
+    filepath HandsomeSoup hspec http-types hxt iso8601-time
+    MissingH multipart silently stm tar text time unix
+    unordered-containers utf8-string wai warp
+    temporary mtl transformers exceptions random
     optparse-applicative
   ];
   homepage = "https://github.com/ryantm/heyefi";
