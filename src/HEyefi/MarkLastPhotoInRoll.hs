74c683443b71abd4fecac73c6b051e0407aac0ff
Ryan Mulligan <ryan@ryantm.com>
2015-07-31 08:04:30 -0700

    work on simplifying soap responses

--- src/HEyefi/MarkLastPhotoInRoll.hs
+++ src/HEyefi/MarkLastPhotoInRoll.hs
@@ -14,19 +14,26 @@ import Text.XML.HXT.Core ( runX
                          , root
                          , writeDocumentToString)
 
+contents body =
+  [ spi t_xml "version=\"1.0\" encoding=\"UTF-8\""
+  , mkelem "SOAP-ENV:Envelope"
+    [ sattr "xmlns:SOAP-ENV" "http://schemas.xmlsoap.org/soap/envelope/" ]
+    [ mkelem "SOAP-ENV:Body" [] body ]]
+
+-- soapResponse :: Control.Monad.IO.Class.MonadIO m =>
+--                 [Control.Arrow.IOStateListArrow.IOSLA
+--                  (Text.XML.HXT.Arrow.XmlState.TypeDefs.XIOState ())
+--                  Text.XML.HXT.DOM.TypeDefs.XmlTree
+--                  Text.XML.HXT.DOM.TypeDefs.XmlTree]
+--                 -> m String
+soapResponse body = do
+  let document = root [] (contents body)
+  fmap head (liftIO (runX (document >>> writeDocumentToString [])))
+
 markLastPhotoInRollResponse :: HEyefiM String
-markLastPhotoInRollResponse = do
-  let document =
-        root []
-        [ spi t_xml "version=\"1.0\" encoding=\"UTF-8\""
-        , mkelem "SOAP-ENV:Envelope"
-          [ sattr "xmlns:SOAP-ENV" "http://schemas.xmlsoap.org/soap/envelope/" ]
-          [ mkelem "SOAP-ENV:Body" []
-            [ mkelem "MarkLastPhotoInRollResponse"
-              [ sattr "xmlns" "http://localhost/api/soap/eyefilm" ]
-              []
-            ]
-          ]
-        ]
-  result <- liftIO (runX (document >>> writeDocumentToString []))
-  return (head result)
+markLastPhotoInRollResponse =
+  soapResponse
+    [ mkelem "MarkLastPhotoInRollResponse"
+      [ sattr "xmlns" "http://localhost/api/soap/eyefilm" ]
+      []
+    ]
