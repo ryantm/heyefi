edb39ef079718c9ae268802a1de9d6c5b3ebfab9
Ryan Mulligan <ryan@ryantm.com>
2015-08-01 08:44:57 -0700

    removed IO from soap response generation

--- src/HEyefi/MarkLastPhotoInRoll.hs
+++ src/HEyefi/MarkLastPhotoInRoll.hs
@@ -4,36 +4,33 @@ module HEyefi.MarkLastPhotoInRoll where
 
 import HEyefi.Types (HEyefiM)
 
-import Control.Arrow ((>>>))
-import Control.Monad.IO.Class (liftIO)
-import Text.XML.HXT.Core ( runX
-                         , mkelem
-                         , spi
-                         , t_xml
-                         , sattr
-                         , root
-                         , writeDocumentToString)
+-- import Text.XML.HXT.Core ( runLA
+--                          , mkelem
+--                          , spi
+--                          , t_xml
+--                          , sattr
+--                          , root
+--                          , writeDocumentToString)
+import Text.XML.HXT.Core
 
+contents :: ArrowXml a => [a n XmlTree] -> [a n XmlTree]
 contents body =
   [ spi t_xml "version=\"1.0\" encoding=\"UTF-8\""
   , mkelem "SOAP-ENV:Envelope"
     [ sattr "xmlns:SOAP-ENV" "http://schemas.xmlsoap.org/soap/envelope/" ]
     [ mkelem "SOAP-ENV:Body" [] body ]]
 
--- soapResponse :: Control.Monad.IO.Class.MonadIO m =>
---                 [Control.Arrow.IOStateListArrow.IOSLA
---                  (Text.XML.HXT.Arrow.XmlState.TypeDefs.XIOState ())
---                  Text.XML.HXT.DOM.TypeDefs.XmlTree
---                  Text.XML.HXT.DOM.TypeDefs.XmlTree]
---                 -> m String
-soapResponse body = do
-  let document = root [] (contents body)
-  fmap head (liftIO (runX (document >>> writeDocumentToString [])))
+soapResponse :: [LA n XmlTree] -> String
+soapResponse body =
+  head (runLA (document >>> writeDocumentToString []) undefined)
+  where
+    document = root [] (contents body)
+
 
 markLastPhotoInRollResponse :: HEyefiM String
 markLastPhotoInRollResponse =
-  soapResponse
+  return (soapResponse
     [ mkelem "MarkLastPhotoInRollResponse"
       [ sattr "xmlns" "http://localhost/api/soap/eyefilm" ]
       []
-    ]
+    ])
