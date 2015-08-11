a57a6bd04cc17c8a794f966e86afe3fb90fad294
Ryan Mulligan <ryan@ryantm.com>
2015-08-11 06:40:18 -0700

    extract App code from Main

---
+++ src/HEyefi/App.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HEyefi.App where
+
+import           HEyefi.Config (runWithConfig)
+import           HEyefi.Log (logDebug)
+import           HEyefi.Soap (handleSoapAction, soapAction)
+import           HEyefi.Strings
+import           HEyefi.Types (SharedConfig, HEyefiApplication)
+import           HEyefi.UploadPhoto (handleUpload)
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import           Data.Maybe (isJust, fromJust, isNothing)
+import           Network.Wai (
+    Application
+  , Request
+  , pathInfo
+  , requestBody
+  , requestMethod
+  , requestHeaders )
+import           Control.Concurrent.STM (
+    atomically
+  , writeTVar
+  , readTVar )
+
+app :: SharedConfig -> Application
+app sharedConfig req f = do
+  config <- atomically (readTVar sharedConfig)
+  body <- getWholeRequestBody req
+  (result, config') <- runWithConfig config (do
+                  logDebug (show (pathInfo req))
+                  logDebug (show (requestHeaders req))
+                  dispatchRequest (BL.fromStrict body) req f)
+  atomically (writeTVar sharedConfig config')
+  return result
+
+dispatchRequest :: BL.ByteString -> HEyefiApplication
+dispatchRequest body req f
+  | requestMethod req == "POST" &&
+    pathInfo req == ["api","soap","eyefilm","v1","upload"] &&
+    isNothing (soapAction req) =
+      handleUpload body req f
+dispatchRequest body req f
+  | requestMethod req == "POST" &&
+    isJust (soapAction req) =
+      handleSoapAction (fromJust (soapAction req)) body req f
+dispatchRequest _ _ _ = error didNotMatchDispatch
+
+getWholeRequestBody :: Request -> IO B.ByteString
+getWholeRequestBody request = do
+  r <- requestBody request
+  if r == B.empty
+    then return B.empty
+    else do
+     rest <- getWholeRequestBody request
+     return (B.append r rest)
--- src/HEyefi/Main.hs
+++ src/HEyefi/Main.hs
@@ -2,14 +2,13 @@
 
 module Main where
 
+import           HEyefi.App (app)
 import           HEyefi.CommandLineOptions (handleOptionsThenMaybe)
 import           HEyefi.Config (monitorConfig, newConfig, runWithConfig)
 import           HEyefi.Constant (port, configPath)
-import           HEyefi.Log (logInfoIO, logDebug)
-import           HEyefi.Soap (handleSoapAction, soapAction)
+import           HEyefi.Log (logInfoIO)
 import           HEyefi.Strings
-import           HEyefi.Types (SharedConfig, HEyefiApplication)
-import           HEyefi.UploadPhoto (handleUpload)
+import           HEyefi.Types (SharedConfig)
 
 import           Control.Concurrent (forkIO)
 import           Control.Concurrent.STM (
@@ -19,19 +18,9 @@ import           Control.Concurrent.STM (
   , TVar
   , readTVar )
 import           Control.Monad (forever, void)
-import qualified Data.ByteString as B
-import           Data.ByteString.Lazy (fromStrict)
-import qualified Data.ByteString.Lazy as BL
-import           Data.Maybe (isJust, fromJust, isNothing)
-import           Network.Wai (
-    Application
-  , Request
-  , pathInfo
-  , requestBody
-  , requestMethod
-  , requestHeaders )
 import           Network.Wai.Handler.Warp (run)
 import           System.Posix.Signals (installHandler, sigHUP, Handler( Catch ))
+
 
 handleHup :: TVar (Maybe Int) -> IO ()
 handleHup wakeSig = atomically (writeTVar wakeSig (Just 1))
@@ -64,35 +53,3 @@ runHeyefi = do
 
 main :: IO ()
 main = handleOptionsThenMaybe runHeyefi
-
-app :: SharedConfig -> Application
-app sharedConfig req f = do
-  config <- atomically (readTVar sharedConfig)
-  body <- getWholeRequestBody req
-  (result, config') <- runWithConfig config (do
-                  logDebug (show (pathInfo req))
-                  logDebug (show (requestHeaders req))
-                  dispatchRequest (fromStrict body) req f)
-  atomically (writeTVar sharedConfig config')
-  return result
-
-dispatchRequest :: BL.ByteString -> HEyefiApplication
-dispatchRequest body req f
-  | requestMethod req == "POST" &&
-    pathInfo req == ["api","soap","eyefilm","v1","upload"] &&
-    isNothing (soapAction req) =
-      handleUpload body req f
-dispatchRequest body req f
-  | requestMethod req == "POST" &&
-    isJust (soapAction req) =
-      handleSoapAction (fromJust (soapAction req)) body req f
-dispatchRequest _ _ _ = error didNotMatchDispatch
-
-getWholeRequestBody :: Request -> IO B.ByteString
-getWholeRequestBody request = do
-  r <- requestBody request
-  if r == B.empty
-    then return B.empty
-    else do
-     rest <- getWholeRequestBody request
-     return (B.append r rest)
