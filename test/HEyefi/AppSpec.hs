bb9f6e7327598dc9e7197784908b6ed822070077
Ryan Mulligan <ryan@ryantm.com>
2015-08-14 07:03:39 -0700

    improve matching of MarkLastPhotoInRoll spec

--- test/HEyefi/AppSpec.hs
+++ test/HEyefi/AppSpec.hs
@@ -17,14 +17,18 @@ app' = do
   config <- liftIO newConfig
   return (app config)
 
+sampleMarkLastPhotoInRoll :: ResponseMatcher
+sampleMarkLastPhotoInRoll = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Body><MarkLastPhotoInRollResponse xmlns=\"http://localhost/api/soap/eyefilm\"/></SOAP-ENV:Body></SOAP-ENV:Envelope>"
+
 spec :: Spec
 spec = do
   with app' (
     do
       describe "MarkLastPhotoInRoll" (
-        it "should respond with status 200"
-        (request
-         methodPost "/"
-         [(CI.mk "SoapAction",  "\"urn:MarkLastPhotoInRoll\"")]
-         ""
-         `shouldRespondWith` 200)))
+        it "should respond with status 200" (
+           do
+             let r = request
+                     methodPost "/"
+                     [(CI.mk "SoapAction",  "\"urn:MarkLastPhotoInRoll\"")]
+                     ""
+             r `shouldRespondWith` sampleMarkLastPhotoInRoll {matchStatus = 200})))
