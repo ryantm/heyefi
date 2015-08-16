1b3aa9274a57193a897816b0994cf923824dfcdb
Ryan Mulligan <ryan@ryantm.com>
2015-08-16 15:39:11 -0700

    add startsession spec at App level

--- test/HEyefi/AppSpec.hs
+++ test/HEyefi/AppSpec.hs
@@ -2,6 +2,7 @@
 
 module HEyefi.AppSpec where
 
+import Control.Concurrent.STM
 import Data.CaseInsensitive as CI
 import Network.HTTP.Types.Method
 import Network.Wai (Application)
@@ -13,21 +14,6 @@ import HEyefi.App
 import HEyefi.Config
 
 
-app' :: IO Application
-app' = do
-  config <- liftIO newConfig
-  return (app config)
-
-sampleMarkLastPhotoRequest :: WaiSession SResponse
-sampleMarkLastPhotoRequest =
-  request
-  methodPost "/"
-  [(CI.mk "SoapAction",  "\"urn:MarkLastPhotoInRoll\"")]
-  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"EyeFi/SOAP/EyeFilm\"><SOAP-ENV:Body><ns1:MarkLastPhotoInRoll><macaddress>001856417729</macaddress><mergedelta>0</mergedelta></ns1:MarkLastPhotoInRoll></SOAP-ENV:Body></SOAP-ENV:Envelope>"
-
-sampleMarkLastPhotoInRoll :: ResponseMatcher
-sampleMarkLastPhotoInRoll = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Body><MarkLastPhotoInRollResponse xmlns=\"http://localhost/api/soap/eyefilm\"/></SOAP-ENV:Body></SOAP-ENV:Envelope>"
-
 spec :: Spec
 spec = do
   with app' (
@@ -37,4 +23,34 @@ spec = do
            do
              sampleMarkLastPhotoRequest
                `shouldRespondWith`
-               sampleMarkLastPhotoInRoll {matchStatus = 200})))
+               sampleMarkLastPhotoInRollResponse {matchStatus = 200}))
+      describe "StartSession" (
+        it "should respond with status 200" (
+           do
+             sampleStartSessionRequest
+               `shouldRespondWith` 200)))
+
+app' :: IO Application
+app' = do
+  sharedConfig <- atomically (newTVar (insertCard "0018562de4ce" "36d61e4e7403a0586702c9159892a062" emptyConfig))
+  return (app sharedConfig)
+
+sampleMarkLastPhotoRequest :: WaiSession SResponse
+sampleMarkLastPhotoRequest =
+  request
+  methodPost "/"
+  [(CI.mk "SoapAction",  "\"urn:MarkLastPhotoInRoll\"")]
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"EyeFi/SOAP/EyeFilm\"><SOAP-ENV:Body><ns1:MarkLastPhotoInRoll><macaddress>001856417729</macaddress><mergedelta>0</mergedelta></ns1:MarkLastPhotoInRoll></SOAP-ENV:Body></SOAP-ENV:Envelope>"
+
+sampleMarkLastPhotoInRollResponse :: ResponseMatcher
+sampleMarkLastPhotoInRollResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Body><MarkLastPhotoInRollResponse xmlns=\"http://localhost/api/soap/eyefilm\"/></SOAP-ENV:Body></SOAP-ENV:Envelope>"
+
+sampleStartSessionRequest :: WaiSession SResponse
+sampleStartSessionRequest =
+  request
+  methodPost "/"
+  [(CI.mk "SoapAction",  "\"urn:StartSession\"")]
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"EyeFi/SOAP/EyeFilm\"><SOAP-ENV:Body><ns1:StartSession><macaddress>0018562de4ce</macaddress><cnonce>6eb0444343c1953e47fb28181bb4e47f</cnonce><transfermode>34</transfermode><transfermodetimestamp>1356903384</transfermodetimestamp></ns1:StartSession></SOAP-ENV:Body></SOAP-ENV:Envelope>"
+
+sampleStartSessionResponse :: ResponseMatcher
+sampleStartSessionResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Body><StartSessionResponse xmlns=\"http://localhost/api/soap/eyefilm\"><credential>f9d03ddcce53582ff10075577e522373</credential><snonce>a0f6fc3983454d6da100c8ab5f3efa12</snonce><transfermode>34</transfermode><transfermodetimestamp>1356903384</transfermodetimestamp><upsyncallowed>true</upsyncallowed></StartSessionResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>"
--- test/HEyefi/SoapSpec.hs
+++ test/HEyefi/SoapSpec.hs
@@ -8,9 +8,6 @@ import HEyefi.Soap
 import HEyefi.Types
 
 
-sampleStartSessionRequest :: String
-sampleStartSessionRequest = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"EyeFi/SOAP/EyeFilm\"><SOAP-ENV:Body><ns1:StartSession><macaddress>0018562de4ce</macaddress><cnonce>6eb0444343c1953e47fb28181bb4e47f</cnonce><transfermode>34</transfermode><transfermodetimestamp>1356903384</transfermodetimestamp></ns1:StartSession></SOAP-ENV:Body></SOAP-ENV:Envelope>"
-
 sampleGetPhotoRequest :: String
 sampleGetPhotoRequest = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"EyeFi/SOAP/EyeFilm\"><SOAP-ENV:Body><ns1:GetPhotoStatus><credential>7daa9ecf3a9f135f5bb30541ed84fcfb</credential><macaddress>0018562de4ce</macaddress><filename>IMG_2195.JPG.tar</filename><filesize>125952</filesize><filesignature>736ffb7fa20f1708fd300c58c0aabb61</filesignature><flags>4</flags></ns1:GetPhotoStatus></SOAP-ENV:Body></SOAP-ENV:Envelope>"
 
